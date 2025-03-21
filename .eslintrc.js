bdfefd0333c3e39f504f628822110e2b09ab86a1
LEE DONG GYU <tenenger7125@gmail.com>
2025-03-22 04:30:12 +0900

    [dotori-repo@feat]: init tailwindcss-plugin repo

---
+++ .eslintrc.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+module.exports = {
+  root: true,
+  env: {
+    browser: true,
+    es2020: true,
+    node: true,
+  },
+  extends: ['dotori-base', 'dotori-typescript'],
+  ignorePatterns: ['**/dist', '**/node_modules'],
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: [path.resolve(__dirname, './packages/dotori-tailwindcss-scrollbar/tsconfig.json')],
+    // project: ['./packages/dotori-tailwindcss-scrollbar/tsconfig.json'],
+    tsconfigRootDir: __dirname,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {
+        alwaysTryTypes: true,
+        project: [path.resolve(__dirname, './packages/dotori-tailwindcss-scrollbar/tsconfig.json')],
+      },
+    },
+  },
+};
