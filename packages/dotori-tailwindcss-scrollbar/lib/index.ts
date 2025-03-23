0f53823eeec7b3cf13c549b898c0563366814d24
LEE DONG GYU <tenenger7125@gmail.com>
2025-03-24 00:51:58 +0900

    [dotori-tailwindcss-scrollbar@refactor]: remove unnecessary bind this code

--- packages/dotori-tailwindcss-scrollbar/lib/index.ts
+++ packages/dotori-tailwindcss-scrollbar/lib/index.ts
@@ -3,8 +3,6 @@ import plugin, { PluginAPI } from 'tailwindcss/plugin';
 export const scrollbarPlugin: ReturnType<typeof plugin> = plugin(({ addUtilities, config }: Plugin) => {
   const prefix = config('prefix') as ((className: string) => string) | (string | undefined);
 
-  const bounded = addUtilities.bind(this);
-
   const newUtilities = {
     [`.${typeof prefix === 'function' ? prefix('scrollbar-hidden') : `${prefix}scrollbar-hidden`}`]: {
       '&::-webkit-scrollbar': {
@@ -13,7 +11,7 @@ export const scrollbarPlugin: ReturnType<typeof plugin> = plugin(({ addUtilities
     },
   };
 
-  bounded(newUtilities, ['responsive', 'hover']);
+  addUtilities(newUtilities, ['responsive', 'hover']);
 });
 
 type Plugin = { addUtilities: PluginAPI['addUtilities']; config: PluginAPI['config'] };
